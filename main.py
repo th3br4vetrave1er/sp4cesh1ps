c3c1ffe3e210b514f430f99e606f9ede626db541
th3br4vetrave1er <tazdingodan@gmail.com>
2024-12-01 21:26:37 +0200

    added stars and ship

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import pygame
-
+from os.path import join
+from random import random, randint
 # General setup
 
 pygame.init()
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+pygame.display.set_caption("SP4CESH1PS")
+
 runnig = True
 
+# Plain Surface
+surf = pygame.Surface((100,200))
+surf.fill("darkgreen")
+x = 100
+
+# Importing image
+star_surf = pygame.image.load(join("images","star.png")).convert_alpha()
+player_surf = pygame.image.load(join("images", "player.png")).convert_alpha()
+star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
 while runnig:
     # Event loop here
@@ -16,6 +28,14 @@ while runnig:
             runnig = False
 
     # Draw of a game
+
+    display_surface.fill("darkgray")
+    x += 0.1
+    display_surface.blit(player_surf, (x,150))
+    for pos in star_positions:
+              display_surface.blit(star_surf, pos)
+
+    pygame.display.update()
 
 
 
---
+++ test.py
@@ -0,0 +1,8 @@
+from random import randint
+rand_x = randint(1, 20)
+rand_y = randint(1, 20)
+for i in range(20):
+    print(i)
+
+print(rand_x)
+print(rand_y)
